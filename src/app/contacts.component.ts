ad0081df4d9f91247a6f6e313f2c22d229b709a9
Jesse Palmer <jesselpalmer@gmail.com>
2016-05-22 20:37:03 -0400

    updates contacts component

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from './shared/contact'
 
 @Component({
   moduleId: module.id,
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['contacts.component.css']
 })
 export class ContactsAppComponent {
-  title = 'contacts works!';
+  title: string = 'Contacts';
+  contacts: Contact[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.contacts = [
+      { name: 'Adrian Directive', email: 'adrian.directive@example.com', number: '7035550123' },
+      { name: 'Rusty Component', email: 'rusty.component@example.com', number: '4155550122' },
+      { name: 'Jeff Pipe', email: 'jeff.pipe@example.com', number: '71455501111'},
+      { name: 'Craig Service', email: 'craig.services@example.com', number: '6505550132'}
+    ];
+  }
 }
